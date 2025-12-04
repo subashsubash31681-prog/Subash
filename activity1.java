40f97d478bd05d7732520a115ade3650eb45d129
subashsubash31681-prog <subashsubash31681@gmail.com>
2025-12-04 15:35:44 +0530

    Add files via upload
    activity1

---
+++ activity1.java
@@ -0,0 +1,14 @@
+package activity1;
+
+public class activity1 {
+
+	public static void main(String[] args) {
+		int age=20;
+		         char logo='S';
+		         String name="Subash";
+		         boolean value=true;
+		         System.out.println( age);
+
+	}
+
+}
