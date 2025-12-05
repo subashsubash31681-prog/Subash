ae5e52b884cd89c70b0b30961d9bb1a974835a60
subashsubash31681-prog <subashsubash31681@gmail.com>
2025-12-05 10:47:25 +0530

    Add files via upload
    mobileclass

---
+++ mobileclass.java
@@ -0,0 +1,18 @@
+
+public class mobileclass {
+
+
+	
+	
+	
+	public void takephoto() {
+		System.out.println("take photo");
+	}
+	public static void main(String[] args) {
+		mobileclass obj=new mobileclass();
+		obj.takephoto();
+		
+
+	}
+
+}
